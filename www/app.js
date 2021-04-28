e21a451b972df3b9ea23c3e96e18120fbd53a41c
Owen O'Connor <46877624+oroconnor@users.noreply.github.com>
2021-04-28 13:49:18 -0400

    Add files via upload

---
+++ www/app.js
@@ -0,0 +1,3 @@
+window.onload = function(){ 
+                alert("This demo website is a project for SUNY Ulster CSC132, and is not an official website for the Bard Center for the Study of Air, Land and Water.");
+                }
